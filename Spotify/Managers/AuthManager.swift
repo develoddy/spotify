5e101cebd1d16482e4d3ec228416f748780988b1
Eddy Lujan <eddylujann@gmail.com>
2022-12-22 16:17:38 +0100

    Agregar icono a la app

--- Spotify/Managers/AuthManager.swift
+++ Spotify/Managers/AuthManager.swift
@@ -126,13 +126,14 @@ final class AuthManager {
     }
     
     
-    public func refreshIfNeeded(completion: @escaping (Bool) -> Void ) {
+    // public func refreshIfNeeded(completion: @escaping (Bool) -> Void ) {
+    public func refreshIfNeeded(completion: ((Bool) -> Void)? ) {
         guard !refreshingToken else {
             return
         }
         
         guard shouldRefreshToken else {
-            completion(true)
+            completion?(true)
             return
         }
         guard let refreshToken = self.refreshToken else {
@@ -165,7 +166,7 @@ final class AuthManager {
         let data = basicToken.data(using: .utf8)
         guard let base64String = data?.base64EncodedString() else {
             print("Failure to get base64")
-            completion(false)
+            completion?(false)
             return
         }
         request.setValue("Basic \(base64String)",
@@ -175,7 +176,7 @@ final class AuthManager {
             self?.refreshingToken = false
             guard let data = data,
                   error == nil else {
-                completion(false)
+                completion?(false)
                 return
             }
             
@@ -184,10 +185,10 @@ final class AuthManager {
                 self?.onRefreshBlocks.forEach{ $0(result.accessToken) }
                 self?.onRefreshBlocks.removeAll()
                 self?.cacheToken(result: result)
-                completion(true)
+                completion?(true)
             } catch {
                 print(error.localizedDescription)
-                completion(false)
+                completion?(false)
             }
         }
         task.resume()
