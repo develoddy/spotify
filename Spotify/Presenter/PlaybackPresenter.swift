07e3d2c571841065b7cea15858185778f4dcc3b5
Eddy Lujan <eddylujann@gmail.com>
2022-12-30 15:17:25 +0100

    Player View controller

--- Spotify/Presenter/PlaybackPresenter.swift
+++ Spotify/Presenter/PlaybackPresenter.swift
@@ -7,8 +7,15 @@ final class PlaybackPresenter {
         from viewController: UIViewController,
         track: AudioTrack
     ) {
+        /*
+         En lugar de presentar este controlador de vista diractamente,
+         lo que vamos a hacer es vamos a presentar este controlador de
+         vista envuelto en un controlador de navegacion
+         */
         let vc = PlayerViewController()
-        viewController.present(vc, animated: true, completion: nil)
+        // viewController.present(vc, animated: true, completion: nil)
+        vc.title = track.name
+        viewController.present(UINavigationController(rootViewController: vc), animated: true, completion: nil)
     }
     
     static func startPlayback(
@@ -16,6 +23,7 @@ final class PlaybackPresenter {
         tracks: [AudioTrack]
     ) {
         let vc = PlayerViewController()
-        viewController.present(vc, animated: true, completion: nil)
+        // viewController.present(vc, animated: true, completion: nil)
+        viewController.present(UINavigationController(rootViewController: vc), animated: true, completion: nil)
     }
 }
