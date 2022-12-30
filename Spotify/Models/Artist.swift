67cd529471038f044124cbc3a5037958b497e963
Eddy Lujan <eddylujann@gmail.com>
2022-12-30 08:51:27 +0100

    changed

---
+++ Spotify/Models/Artist.swift
@@ -0,0 +1,19 @@
+//
+//  Artist.swift
+//  Spotify
+//
+//  Created by Eddy Donald Chinchay Lujan on 30/12/22.
+//
+
+import Foundation
+
+struct Artist: Codable {
+    let externalUrls: ExternalUrls
+    let href: String
+    let id, name, type, uri: String
+
+    enum CodingKeys: String, CodingKey {
+        case externalUrls = "external_urls"
+        case href, id, name, type, uri
+    }
+}
